6093b6191afda9378c8381fec1955a58dfaf7064
rosannareibaldi <139228970+rosannareibaldi@users.noreply.github.com>
2023-07-11 16:21:57 +0200

    prima versione

--- gioco.py
+++ gioco.py
@@ -1,5 +1,6 @@
 import random
-# Scelta del livello: facciamo selezionare da utente sia la difficoltà delle parole che il numero di tentativi con cui 
+
+# Scelta del livello: facciamo selezionare da utente sia la difficoltà delle parole che il numero di tentativi con cui
 # giocare
 livello = input("Scegli il livello di difficoltà fra 1, 2, 3:\n1->Facile\n2->Medio\n3->Difficile")
 livello_diff = {
@@ -7,5 +8,41 @@ livello_diff = {
     "2": {"parole": ['castello', 'lampadina', 'scaffale', 'forbice', 'habitat'], "n_tentativi": 7},
     "3": {"parole": ["weekend", "bajour", "disarcivescovizzare"], "n_tentativi": 6}
 }
+
+
 lista_parole = livello_diff[livello]["parole"]
 n_tentativi = livello_diff[livello]["n_tentativi"]
+
+# Scegliamo una parola a caso fra quelle della lista_parole
+parola = random.choice(lista_parole)
+
+# definiamo una parola che tiene traccia dei tentativi utente
+parola_nascosta = list('_'*len(parola))
+
+# Definisco un contatore per stoppare il gioco in caso di troppi tentativi
+contatore = 0
+
+# Ciclo while: continua finchè ho tentativi rimanenti e non ho indovinato la parola
+while contatore < n_tentativi and '_' in parola_nascosta:
+    print(f"parola da indovinare: {''.join(parola_nascosta)}")
+
+    # Chiediamo di inserire una lettera all'utente
+    lettera = input("Inserisci una lettera con cui giocare: ").lower()
+
+    # Se la lettera è presente...
+    if lettera in parola:
+        # Cerchiamo gli indici e sostituiamo la lettera nella posizione dove è presente
+        for i, l in enumerate(parola):
+            if l == lettera:
+                parola_nascosta[i] = lettera
+        print("Bravo, la lettera è presente nella parola!\n")
+
+    else:
+        contatore += 1
+        print(f"Lettera non presente. Hai ancora {n_tentativi - contatore} tentativi.\n")
+
+
+if parola_nascosta == list(parola):
+    print(f"Ottimo, hai vinto! La parola era: {parola}")
+else:
+    print(f"Tentativi finiti, hai perso. La parola era: {parola}")
